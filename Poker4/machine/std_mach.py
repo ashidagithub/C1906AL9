e342f997bedd12ca494814ea0a7d1122d93d8982
ashidagithub <1019726435@qq.com>
2019-07-18 21:34:53 +0800

    updated csv lesson

--- Poker4/machine/std_mach.py
+++ Poker4/machine/std_mach.py
@@ -14,6 +14,7 @@ import codecs
 import os
 import csv
 
+
 def create_deck_54(new_deck):
     '推出一副54张的新牌'
     print('\n -- debug: I made a new deck of 54.')
@@ -36,19 +37,19 @@ def create_deck_54(new_deck):
 
     return
 
+
 def create_deck_52(new_deck):
     '推出一副52张的新牌'
     print('\n -- debug: I made a new deck of 52.')
 
     # initialize var
     #cardJokers = ('♞', '♘')
+    # for c in cardJokers:
+    #    new_deck.append(c)
+
     cardMarks = ('♠', '♥', '♣', '♦')
     cardNumbers = ('2', '3', '4', '5', '6', '7', '8',
                    '9', '10', 'J', 'Q', 'K', 'A')
-
-    #for c in cardJokers:
-    #    new_deck.append(c)
-
     # add 4x13 cards
     for cn in cardNumbers:
         for cm in cardMarks:
@@ -58,6 +59,7 @@ def create_deck_52(new_deck):
 
     return
 
+
 def shuffled_deck(deck_to_be_shuffled):
     '洗牌'
     print('\n -- debug: I shuffled a deck')
@@ -66,42 +68,38 @@ def shuffled_deck(deck_to_be_shuffled):
     return
 
 
+'''
 def record_deck(deck_to_be_record, filename):
     '记录一副牌'
     print('\n -- debug: I record a deck')
 
-    out_path = os.getcwd() + '\\csv_files\\' + filename
-
-    '''
+    out_path = os.getcwd() + '\\OutputDecks\\' + filename
     #print(out_path)
     f = codecs.open(out_path, "w", "utf-8")
     for card in deck_to_be_record:
         f.write(card)
         f.write('\n')
     f.close
-    '''
-    with open(out_path, "w", encoding='utf-8', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(deck_to_be_record)
 
     return
+'''
 
-#---- Poker 3.0 added
+# ---- Poker 3.0 added
 def make_deck_by_type(play_type, out_deck):
     '按要求制作各种扑克牌'
-    if play_type == 1: # 争上游的牌
+    if play_type == 1:  # 争上游的牌
         create_deck_54(out_deck)
         shuffled_deck(out_deck)
-        record_deck(out_deck,'争上游-刚洗好的牌.txt')
-    if play_type == 2: # 桥牌
+        record_deck(out_deck, '争上游-刚洗好的牌.txt')
+    if play_type == 2:  # 桥牌
         create_deck_52(out_deck)
         shuffled_deck(out_deck)
-        record_deck(out_deck,'桥牌-刚洗好的牌.txt')
-    if play_type == 3: # 三人斗地主的牌
+        record_deck(out_deck, '桥牌-刚洗好的牌.txt')
+    if play_type == 3:  # 三人斗地主的牌
         create_deck_54(out_deck)
         shuffled_deck(out_deck)
-        record_deck(out_deck,'三人斗地主-刚洗好的牌.txt')
-    if play_type == 4: # 四人斗地主的牌
+        record_deck(out_deck, '三人斗地主-刚洗好的牌.txt')
+    if play_type == 4:  # 四人斗地主的牌
         # first deck
         deck_a = []
         create_deck_54(deck_a)
@@ -112,6 +110,19 @@ def make_deck_by_type(play_type, out_deck):
         out_deck.extend(deck_b)
         # shuffled & record
         shuffled_deck(out_deck)
-        record_deck(out_deck,'四人斗地主-刚洗好的牌.txt')
+        record_deck(out_deck, '四人斗地主-刚洗好的牌.txt')
+
+    return
+
+# ---- Poker 4.0 added
+def record_deck_csv(deck_to_be_record, csv_filename):
+    '用CSV格式记录一副牌'
+    print('\n -- debug: I record a deck')
+
+    out_path = os.getcwd() + '\\out_decks_csv\\' + csv_filename
+
+    with open(out_path, "w", encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(deck_to_be_record)
 
     return
--- Poker4/main_csv_deck.py
+++ Poker4/main_csv_deck.py
@@ -13,8 +13,8 @@ from machine.std_mach import *
 
 deck = []
 create_deck_52(deck)
-record_deck(deck, '52张扑克牌.csv')
+record_deck_csv(deck, '52张扑克牌.csv')
 
 deck = []
 create_deck_54(deck)
-record_deck(deck, '54张扑克牌.csv')
+record_deck_csv(deck, '54张扑克牌.csv')
